3fe16b74438a496630f3ace529f8f2e1ae854907
Tucker Chapman <tucker.r.chapman@gmail.com>
2021-11-16 14:06:54 -0500

    Add 7 fuzz for all cases

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,10 @@
 def calc_fizz_buzz(number):
     if (number % 3 == 0 and number % 5 == 0 and number % 7 == 0):
         return "fizzbuzzfuzz"
+    if (number % 3 == 0 and number % 7 == 0):
+        return "fizzfuzz"
+    if (number % 5 == 0 and number % 7 == 0):
+        return "buzzfuzz"
     if (number % 3 == 0 and number % 5 == 0):
         return "fizzbuzz"
     if (number % 7 == 0):
@@ -15,5 +19,5 @@ def calc_fizz_buzz(number):
 
 
 if __name__ == "__main__":
-    for i in range(1, 26):
+    for i in range(1, 110):
         print(calc_fizz_buzz(i))
