945026e9f21056460ee6e9195e81f60b5caf5dd5
Tucker Chapman <tucker.r.chapman@gmail.com>
2021-11-16 14:01:44 -0500

    Add 7 fuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
 
 def calc_fizz_buzz(number):
+    if (number % 3 == 0 and number % 5 == 0 and number % 7 == 0):
+        return "fizzbuzzfuzz"
     if (number % 3 == 0 and number % 5 == 0):
         return "fizzbuzz"
+    if (number % 7 == 0):
+        return "fuzz"
     if (number % 3 == 0):
         return "fizz"
     elif (number % 5 == 0):
